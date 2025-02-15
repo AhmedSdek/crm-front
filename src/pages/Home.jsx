ab6bd85d11bde7a12b9e4a7751956a8428153052
AhmedSdek <93591832+AhmedSdek@users.noreply.github.com>
2025-02-15 10:08:01 +0200

    h

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+    const [user, setUser] = useState(null);
+    const token = localStorage.getItem('token');
+    const nav = useNavigate()
+    useEffect(() => {
+        if (token) {
+            try {
+                const decodedUser = jwtDecode(token);
+                console.log(decodedUser)
+                setUser(decodedUser);
+                // if () {
+
+                // }
+            } catch (error) {
+                console.error("Invalid token:", error);
+                setUser(null);
+            }
+        }
+    }, [token]);
+    // useEffect(() => {
+
+    // }, []);
     return (
         <div>Home</div>
     )
