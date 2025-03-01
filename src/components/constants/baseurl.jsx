b2832dda48cdc33c07db6d08bb9adccbd82878eb
AhmedSdek <93591832+AhmedSdek@users.noreply.github.com>
2025-03-01 14:23:41 +0200

    لا

--- src/components/constants/baseurl.jsx
+++ src/components/constants/baseurl.jsx
@@ -1,3 +1,5 @@
 // URL الأساسي
 // export const BASE_URL = 'http://localhost:5000';
-export const BASE_URL = 'https://crm-back-production-32bf.up.railway.app';
+// export const BASE_URL = 'https://crm-back-production-32bf.up.railway.app';
+export const BASE_URL = 'https://still-dulcine-ahmedsdek-11febc37.koyeb.app';
+
