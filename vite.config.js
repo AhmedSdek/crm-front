6e01ee285d4fa501cc40a3e408f75ba3ff88cd03
AhmedSdek <93591832+AhmedSdek@users.noreply.github.com>
2025-04-04 20:31:20 +0200

    def

--- vite.config.js
+++ vite.config.js
@@ -19,18 +19,39 @@ export default defineConfig({
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#1976d2',
-        icons: [
+        "id": "/",
+        "icons": [
           {
-            src: '/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            "src": "/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
           },
           {
-            src: '/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            "src": "/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
           },
+          {
+            "src": "/icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          }
         ],
+        "screenshots": [
+          {
+            "src": "/Screenshot (187).png",
+            "sizes": "1280x720",
+            "type": "image/png",
+            "form_factor": "wide"
+          },
+          {
+            "src": "/Screenshot (188).png",
+            "sizes": "720x1280",
+            "type": "image/png",
+            "form_factor": "narrow"
+          }
+        ]
+
       },
     }),
   ]
